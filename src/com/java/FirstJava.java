ca2b5b2d650fe4e2758ee54769f110991465567b
araviteja <atluri.raviteja005@gmail.com>
2016-05-16 17:24:39 +0530

    commit

---
+++ src/com/java/FirstJava.java
@@ -0,0 +1,11 @@
+package com.java;
+
+public class FirstJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Project Java Class");
+
+	}
+
+}

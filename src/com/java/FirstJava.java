baf0b8be77c214346db877c35bf51b2f880b9f01
araviteja <atluri.raviteja005@gmail.com>
2016-05-23 15:03:04 +0530

    java

--- src/com/java/FirstJava.java
+++ src/com/java/FirstJava.java
@@ -5,6 +5,10 @@ public class FirstJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Project Java Class");
+		String s1="ravitjea";
+		String s2="ravitjea";
+		
+FirstJava f1=new FirstJava();
 
 	}
 
